f6643c550bea4e516b25275d024a91176ed02575
Ali Hamza <alihamza022bb@gmail.com>
2023-03-12 15:52:53 -0700

    reponsive

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// let mybutton = document.querySelector(".scroll-btn");
-
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
 
 const hamburger = document.querySelector(".hamburger")
 const navbar = document.querySelector(".navbar")
