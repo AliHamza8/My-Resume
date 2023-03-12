1a4db2dea6b61ae480d29c81f183596b2c18379b
Ali Hamza <alihamza022bb@gmail.com>
2023-03-12 12:54:39 -0700

    hamburger menu added

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let mybutton = document.querySelector(".scroll-btn");
+// let mybutton = document.querySelector(".scroll-btn");
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+const hamburger = document.querySelector(".hamburger")
+const navbar = document.querySelector(".navbar")
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active")
+  navbar.classList.toggle("active")
+})
+
+document.querySelector(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active")
+  navbar.classList.remove("active")
+}))
